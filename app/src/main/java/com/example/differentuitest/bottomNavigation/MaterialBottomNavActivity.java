ad848d186aff0adc7c0c967db8195b6c7008fb7c
TANVIR <rockingtanvir@gmail.com>
2022-06-26 12:17:54 +0600

    bottom nav activities added

---
+++ app/src/main/java/com/example/differentuitest/bottomNavigation/MaterialBottomNavActivity.java
@@ -0,0 +1,16 @@
+package com.example.differentuitest.bottomNavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.differentuitest.R;
+
+public class MaterialBottomNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_bottom_nav);
+    }
+}
