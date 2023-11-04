b19ae88cda967c03527d1ab8613531fbb24b3df1
TANVIR <rockingtanvir@gmail.com>
2023-11-04 17:04:13 +0600

    expandable Card Activity id added added

--- app/src/main/java/com/example/differentuitest/OCRActivity.java
+++ app/src/main/java/com/example/differentuitest/OCRActivity.java
@@ -1,14 +1,22 @@
 package com.example.differentuitest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class OCRActivity extends AppCompatActivity {
+
+    EditText ocrText;
+    AppCompatButton uploadBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocractivity);
+
+        ocrText = findViewById(R.id.ocrTextId);
+        uploadBtn = findViewById(R.id.uploadBtnId);
     }
 }
